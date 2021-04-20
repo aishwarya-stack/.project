a22dc0972804aab60513972919e65aa267ff05a0
aishwarya-stack <81794479+aishwarya-stack@users.noreply.github.com>
2021-04-20 17:45:46 +0530

    Add files via upload

---
+++ document.js
@@ -0,0 +1,23 @@
+console.log(typeof'abcdef')
+console.log(typeof 20)
+console.log(typeof true)
+console.log(typeof null)
+
+//variable without assign value
+let names
+let address
+let pincode
+let phonenumber
+console.log(names,address,pincode,phonenumber)
+
+//variable with assign value
+console.log(a,b,c,d)
+
+
+
+
+
+let fname='abcd',lastnames='pqrs',countryy='india'
+console.log(fname,lastnames,countryy)
+
+
---
+++ variable.js
@@ -0,0 +1,15 @@
+let first='java'
+let empty=null
+let tname
+console.log(tname)
+console.log(tname,empty)
+let a=100
+let b=200
+let c=300
+let d=400
+let firstname='abcd'
+
+let lastname='pqrs'
+
+let country='india'
+
